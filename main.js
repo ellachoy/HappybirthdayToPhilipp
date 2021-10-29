f6fd36f8693ef7dfff0c72ab695b4d51fef9e922
Ella Choy <egal21de@gmail.com>
2021-10-29 16:42:47 +0200

    correction

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-$(document).ready(() => {
-    let typingElement = $(".typing");
+// $(document).ready(() => {
+//     let typingElement = $(".typing");
   
-    typingElement.on("click", (e) => {
-      typingElement.removeClass("animate");
-      setTimeout(() => typingElement.addClass("animate"), 1);
-    });
-  });
+//     typingElement.on("click", (e) => {
+//       typingElement.removeClass("animate");
+//       setTimeout(() => typingElement.addClass("animate"), 1);
+//     });
+//   });
